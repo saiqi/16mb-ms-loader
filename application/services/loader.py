3f88fc66a364ede756e22c4a22896f89857f5bf5
Julien Bernard <julien.bernard.iphone@gmail.com>
2019-05-21 21:46:11 +0100

    log, alpine & log format

--- application/services/loader.py
+++ application/services/loader.py
@@ -1,6 +1,9 @@
+import logging
+
 from nameko.rpc import rpc, RpcProxy
 import bson.json_util
 
+_log = logging.getLogger(__name__)
 
 class LoaderServiceError(Exception):
     pass
@@ -15,16 +18,20 @@ class LoaderService(object):
 
     @rpc
     def write(self, write_policy, meta, target_table, records, upsert_key=None, delete_keys=None, chunk_size=None):
+        _log.info(f'Writing in {target_table} using {write_policy} strategy ...')
         if write_policy not in ('insert', 'upsert', 'bulk_insert', 'delete_insert', 'delete_bulk_insert',
                                 'truncate_insert', 'truncate_bulk_insert'):
+            _log.error(f'{write_policy} not supported')
             raise LoaderServiceError('Wrong value for parameter write_policy')
 
         if write_policy in ('bulk_insert', 'delete_bulk_insert', 'truncate_bulk_insert') and not chunk_size:
+            _log.error('chunk_size missing')
             raise LoaderServiceError('Bulk loading strategy requires a chunk size')
 
         try:
             meta = list(map(tuple, meta))
         except:
+            _log.error('Bad formated meta')
             raise LoaderServiceError('Bad formated meta')
 
         if write_policy == 'insert':
@@ -46,38 +53,54 @@ class LoaderService(object):
             self.datastore.truncate(target_table)
             self.datastore.bulk_insert(target_table, records, meta, chunk_size=chunk_size)
 
+        _log.info('Datastore microservice wrote all the records !')
         return {'target_table': target_table, 'count': len(records)}
 
-    def compute_transformation(self, t, param_value=None):
+    def _compute_transformation(self, t, param_value=None):
+        _log.info(f"Computing transformation {t['id']}")
         try:
             self.datastore.create_or_replace_python_function(t['function_name'], t['function'])
         except:
+            _log.error('Something went wrong while creating the underlying python function')
             raise LoaderServiceError('An error occured while creating python function in transformation {}'.format(t['id']))
 
         if t['type'] == 'fit' and t['process_date'] is None:
+            _log.info('Transformation has been set as \"fit\" kind. This must be processed !')
             try:
                 last_entry = bson.json_util.loads(self.datareader.select(t['output']))
                 if last_entry and len(last_entry) > 0:
+                    _log.info('Deleting the previous result ...')
                     self.datastore.delete(t['target_table'], {'id': last_entry[0]['id']})
+                _log.info('Computing current result ...')
                 self.datastore.insert_from_select(t['target_table'], t['output'], None)
             except:
+                _log.error('Something went wrong while deleting and inserting the result')
                 raise LoaderServiceError('An error occured while fitting transformation {}'.format(t['id']))
+            _log.info('Updating process date in metadata ...')
             self.metadata.update_process_date(t['id'])
         elif t['type'] in ('transform', 'predict',) and t['materialized'] is True:
+            _log.info('Transformation has been set as materialized \"tranform\" or \"predict\" kind. This must be processed !')
             try:
                 if t['parameters'] is None:
+                    _log.info('No parameters truncating the table ...')
                     self.datastore.truncate(t['target_table'])
+                    _log.info('Inserting the result ...')
                     self.datastore.insert_from_select(t['target_table'], t['output'], None)
                 else:
                     if len(t['parameters']) > 1:
+                        _log.error('Multiple parameters has been provided which is currently not supported !')
                         raise BadRequest('Does not support transformation with multiple parameters')
                     param_name = t['parameters'][0]
                     if param_value is None:
+                        _log.error('The transformation requires a parameter which looks empty or missing !')
                         raise BadRequest('Transformation requires a parameter')
+                    _log.info('No parameters. We will delete the previous result according to the provided parameter')
                     self.datastore.delete(t['target_table'], {param_name: param_value})
+                    _log.info('Inserting the result ...')
                     self.datastore.insert_from_select(t['target_table'], t['output'], [param_value])
             except:
                 raise LoaderServiceError('An error occured while computing transformation {}'.format(t['id']))
+            _log.info('Updating process date in metadata ...')
             self.metadata.update_process_date(t['id'])
 
     @rpc
